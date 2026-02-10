9e560d832328892b27a7b824ce0b294cead4a036
Jon Craton <jncraton@gmail.com>
2026-02-10 15:36:52 -0500

    Add doctests

--- examples/calc-parser/parse.py
+++ examples/calc-parser/parse.py
@@ -1,13 +1,22 @@
 import sys
 import re
+import doctest
 
 import pprint
 pp = pprint.PrettyPrinter(indent=2)
 
 def lex_one(value):
-    if re.match('\d+', value):
+    """
+    >>> lex_one('123')
+    ('number', '123')
+    >>> lex_one('+')
+    ('operator', '+')
+    >>> lex_one('!')
+    ('postfix_unary_operator', '!')
+    """
+    if re.match(r'\d+', value):
         return ('number', value)
-    elif re.match('[\+\-\*\/]', value):
+    elif re.match(r'[\+\-\*\/]', value):
         return ('operator', value)
     elif re.match('!', value):
         return ('postfix_unary_operator', value)
@@ -15,13 +24,22 @@ def lex_one(value):
         raise ValueError(f'Invalid token: {value}')
 
 def lex(text):
+    """
+    >>> list(lex('1 + 2'))
+    [(0, 'number', '1'), (1, 'operator', '+'), (2, 'number', '2')]
+    """
     for i, v in enumerate(text.split()):
         token = lex_one(v)
         yield (i, token[0], token[1])
 
 def reduce(parse_tree, rules):
-    """ 
-    Reduce a parse tree by applying grammar rules
+    """
+    >>> rules = [('number', ('number', 'operator', 'number'))]
+    >>> tree = [[(0, 'number', '1')], [(1, 'operator', '+')], [(2, 'number', '2')]]
+    >>> reduce(tree, rules)
+    True
+    >>> tree
+    [[(1, 'number'), [(0, 'number', '1')], [(1, 'operator', '+')], [(2, 'number', '2')]]]
     """
     for lhs, rhs in rules:
         if tuple([t[0][1] for t in parse_tree[-len(rhs):]]) == rhs:
@@ -33,14 +51,16 @@ def reduce(parse_tree, rules):
             return True
 
 def parse(tokens, rules):
-    """ 
-    An LR shift-reduce parser
+    """
+    >>> rules = [('number', ('number', 'operator', 'number'))]
+    >>> parse(lex('1 + 2'), rules)
+    [[(1, 'number'), [(0, 'number', '1')], [(1, 'operator', '+')], [(2, 'number', '2')]]]
     """
     
     parse_tree = []
 
     for token in tokens:
-        # Shift token on to parse tree
+        # Shift token on to parse tree
         parse_tree.append([token])
 
         # Reduce tokens matching production rules
@@ -53,9 +73,11 @@ def parse(tokens, rules):
     return parse_tree
 
 if __name__ == '__main__':
-    rules = [
-        ('number', ('number', 'operator', 'number')),
-        ('number', ('number', 'postfix_unary_operator')),
-    ]
-    
-    pp.pprint(parse(lex(sys.argv[1]), rules))
+    doctest.testmod()
+    if len(sys.argv) > 1:
+        rules = [
+            ('number', ('number', 'operator', 'number')),
+            ('number', ('number', 'postfix_unary_operator')),
+        ]
+        
+        pp.pprint(parse(lex(sys.argv[1]), rules))
