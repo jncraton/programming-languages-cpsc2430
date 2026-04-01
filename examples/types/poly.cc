a4e5d79eba42b6ca709bc08672c703ae48312b03
Jon Craton <jncraton@gmail.com>
2026-04-01 12:13:58 -0400

    Correct string comparison

--- examples/types/poly.cc
+++ examples/types/poly.cc
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <cstring>
 
 template <typename Type>
-Type get_min (Type a, Type b) {
-   return a < b ? a:b;
+Type get_min(Type a, Type b) {
+  return a < b ? a : b;
 }
 
-int main () {
-   std::cout << get_min(2, 1) << std::endl;
-   std::cout << get_min("hello", "world") << std::endl;
+template <>
+const char* get_min<const char*>(const char* a, const char* b) {
+  return std::strcmp(a, b) < 0 ? a : b;
+}
+
+int main() {
+  std::cout << get_min(2, 1) << std::endl;
+  std::cout << get_min("hello", "world") << std::endl;
 }
